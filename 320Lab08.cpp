e1857401b181ad6c96b619e4dc68cf84a20ba168
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-04-03 15:25:15 -0400

    Update 320Lab08.cpp

--- 320Lab08.cpp
+++ 320Lab08.cpp
@@ -1,1 +1,31 @@
+#include "d_pqueue.h"
+#include "preqrec.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+srand(time(0));
+miniPQ<procReqRec> mpq;
+char process = 'A';
+for(int i = 0; i < 10; i++){
+int priority = rand() % 40;
+string name = "Process ";
+name = name + process;
+procReqRec newReq = procReqRec(name,priority);
+mpq.push(newReq);
+process++;
+}
+for(int i = 0; i < 10; i++){
+    cout << mpq.top();
+    mpq.pop();
+}
+
+
+
+
+
+    return 0;
+}
 
